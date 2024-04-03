c98a01f342f8ad0bf9b2d690266b4743d242d077
Fivekahin <trentonnj50@gmail.com>
2024-04-02 22:05:26 -0400

    Update Main.java

--- Pacman/src/main/java/org/example/Main.java
+++ Pacman/src/main/java/org/example/Main.java
@@ -3,15 +3,64 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+    public static void main(String[] args) {
+
+        public static void main(String[] args) {
+
+
+            int[][] board = {
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+                    {1, 2, 1, 1, 2, 2, 1, 1, 2, 1},
+                    {1, 3, 2, 2, 2, 2, 2, 2, 2, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+            };
+
+            printBoard(board);
+            movePacmanRight(board);
+            System.out.println("After moving Pac-Man to the right:");
+            printBoard(board);
+        }
+
+
+    }
+
+
+    public static void printBoard(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                switch (board[i][j]) {
+                    case 0:
+                        System.out.print("  "); // Empty space
+                        break;
+                    case 1:
+                        System.out.print("W "); // Wall
+                        break;
+                    case 2:
+                        System.out.print(". "); // Dot
+                        break;
+                    case 3:
+                        System.out.print("P "); // Pac-Man
+                        break;
+                }
+            }
+            System.out.println();
         }
     }
+    public static void movePacmanRight(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 3) { // Find Pac-Man
+                    if (j + 1 < board[i].length && board[i][j + 1] != 1) { // Check if right is not a wall
+                        board[i][j] = 0; // Remove Pac-Man from current position
+                        board[i][j + 1] = 3; // Place Pac-Man in new position
+                    }
+                    return; // Exit the method after moving
+                }
+            }
+        }
+    }
+
 }
